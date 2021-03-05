60241d4e3c1e517e111a34821b762e743d8f56dc
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-03-05 23:56:47 +0000

    add integration test module

---
+++ app-with-integration-test-module/integration-test/src/test/java/ExampleTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ExampleTest {
+    @Test
+    void someTest() {
+        assertEquals(1, 1);
+    }
+}
